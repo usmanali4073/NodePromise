39216bc461475cae9c78499b745b357eb306c877
usman ali <usmanali4073@gmail.com>
2019-12-09 20:49:01 -0500

    working Promiss with then and error

--- index.js
+++ index.js
@@ -8,22 +8,26 @@ var request = require('request-promise');
 var getWeather = function() {
     return request({
         "method": "GET",
-        "uri": "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22",
+        "uri": "https://openweathermap.org/data/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22",
         "json": true
+    }).catch((err) => {
+        console.log(err.error)
     });
+
 
 }
 
 
 app.get('/', async(req, res) => {
 
-    await getWeather().then(data => {
-        console.log(data)
-        return res.json(data)
-    }).catch((err) => {
-        console.log("failed to get data")
-        return res.statusCode(300)
-    })
+    await getWeather()
+        .then(data => {
+            console.log(data)
+            return res.json(data)
+        }).catch((err) => {
+            console.log("failed to get data")
+            return res.statusCode(300)
+        })
 })
 
 
