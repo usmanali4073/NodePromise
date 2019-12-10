d8a002d0d3cc00318af58d05cb2d36697150d5bc
usman ali <usmanali4073@gmail.com>
2019-12-09 21:13:57 -0500

    Promies with Async and await

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan');
 const port = 3000
+
+morgan('short')
 
 var request = require('request-promise');
 
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-var getWeather = function() {
+var getWeatherbyCite = function() {
     return request({
         "method": "GET",
-        "uri": "https://openweathermap.org/data/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22",
+        "uri": "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22",
         "json": true
     }).catch((err) => {
-        console.log(err.error)
+        console.log(err)
     });
-
-
 }
+var getWeatherCountry = function() {
+    return request({
+        "method": "GET",
+        "uri": "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22",
+        "json": true
+    }).catch((err) => {
+        console.log(err)
+    });
+}
+
 
 
 app.get('/', async(req, res) => {
 
-    await getWeather()
+    let weatherdata = [];
+    await getWeatherbyCite()
         .then(data => {
             console.log(data)
-            return res.json(data)
+            weatherdata.push(data);
         }).catch((err) => {
             console.log("failed to get data")
             return res.statusCode(300)
         })
+    await getWeatherCountry()
+        .then(data => {
+            console.log(data)
+            weatherdata.push(data);
+        }).catch((err) => {
+            console.log("failed to get data")
+            return res.statusCode(300)
+        })
+    return res.json(weatherdata)
 })
-
-
-
-
 
 
 
